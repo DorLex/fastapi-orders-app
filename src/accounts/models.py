649aa1ca8f27b3fcb1b8ff0728572a67bc0f9a70
DorLex <dorozhinskiysanya@gmail.com>
2024-02-15 16:04:19 +0300

    .

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(100))
+    username: Mapped[str] = mapped_column(String(100), unique=True)
 
     hashed_password: Mapped[str] = mapped_column(String)
 
