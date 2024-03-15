6a89b96c4c300df39b7e83b779e9f79b89cde67c
DorLex <dorozhinskiysanya@gmail.com>
2024-03-15 23:30:58 +0300

    add auth fixture

--- tests/accounts/test_users.py
+++ tests/accounts/test_users.py
@@ -2,7 +2,6 @@ from starlette import status
 
 
 class TestUsers:
-    def test_read_users(self, client):
-        response = client.get('/users')
-        assert response.status_code == status.HTTP_200_OK
-        # assert response.json() == [{'username': 'alex', 'id': 1}]
+    def test_read_users(self, client, auth_headers):
+        response = client.get('/users', headers=auth_headers)
+        assert response.status_code == status.HTTP_200_OK, response.text
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from starlette import status
 from starlette.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -31,3 +32,29 @@ app.dependency_overrides[get_db] = override_get_db
 @pytest.fixture(scope='session')
 def client():
     return TestClient(app=app)
+
+
+@pytest.fixture(scope='session')
+def base_test_user_data():
+    return {
+        'username': 'base_test_user',
+        'password': '123456789',
+    }
+
+
+@pytest.fixture(scope='session')
+def create_base_test_user(prepare_db, client, base_test_user_data):
+    response = client.post('/register', json=base_test_user_data)
+    assert response.status_code == status.HTTP_201_CREATED, response.text
+
+
+@pytest.fixture(scope='session')
+def access_token(create_base_test_user, client, base_test_user_data):
+    response = client.post('/auth/token', data=base_test_user_data)
+    assert response.status_code == status.HTTP_200_OK, response.text
+    return response.json().get('access_token')
+
+
+@pytest.fixture(scope='session')
+def auth_headers(access_token):
+    return {'Authorization': f'Bearer {access_token}'}
