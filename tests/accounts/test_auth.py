eb86d13e4a45265d87dac70ca73a24c5c1eb8307
DorLex <dorozhinskiysanya@gmail.com>
2024-03-15 22:22:01 +0300

    add test login

---
+++ tests/accounts/test_auth.py
@@ -0,0 +1,13 @@
+from starlette import status
+
+
+def test_login_for_access_token(client):
+    user_data = {
+        'username': 'test_user_for_auth',
+        'password': '123456789',
+    }
+
+    client.post('/register', json=user_data)
+    response = client.post('/auth/token', data=user_data)
+
+    assert response.status_code == status.HTTP_200_OK, response.text
