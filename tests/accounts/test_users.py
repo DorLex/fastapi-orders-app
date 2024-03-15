23e3f592feb83d938ec08bd9dbf3b154d99786df
DorLex <dorozhinskiysanya@gmail.com>
2024-03-15 19:36:44 +0300

    prepare test db

--- tests/accounts/test_users.py
+++ tests/accounts/test_users.py
@@ -4,4 +4,5 @@ from starlette import status
 class TestUsers:
     def test_read_users(self, client):
         response = client.get('/users')
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.status_code == status.HTTP_200_OK
+        # assert response.json() == [{'username': 'alex', 'id': 1}]
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,28 @@ from starlette.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from src.database import Base
+from src.dependencies import get_db
 from src.main import app
 from .config import DATABASE_URL_TEST
 
 engine_test = create_engine(DATABASE_URL_TEST)
 SessionTest = sessionmaker(engine_test, autocommit=False, autoflush=False)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def prepare_db():
+    Base.metadata.create_all(bind=engine_test)
+    yield
+    Base.metadata.drop_all(bind=engine_test)
+
+
+def override_get_db():
+    with SessionTest() as db:
+        yield db
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 
 @pytest.fixture(scope='session')
