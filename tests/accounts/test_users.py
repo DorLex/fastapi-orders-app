318b09a8767f6852378bd27e2bc1f5ca227c3d5c
DorLex <dorozhinskiysanya@gmail.com>
2024-03-16 16:35:52 +0300

    update test users

--- tests/accounts/test_users.py
+++ tests/accounts/test_users.py
@@ -5,3 +5,8 @@ class TestUsers:
     def test_read_users(self, client, auth_headers):
         response = client.get('/users', headers=auth_headers)
         assert response.status_code == status.HTTP_200_OK, response.text
+
+    def test_read_users_me(self, client, auth_headers, base_test_user_data):
+        response = client.get('/users/me/', headers=auth_headers)
+        assert response.status_code == status.HTTP_200_OK, response.text
+        assert response.json().get('username') == base_test_user_data.get('username')
