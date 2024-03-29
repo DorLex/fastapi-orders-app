112c75f072f285f01e69427544d1022f33b87f03
DorLex <dorozhinskiysanya@gmail.com>
2024-03-29 18:25:17 +0300

    refactor(order_processing): refact OrderProcessingService

--- src/order_processing/service.py
+++ src/order_processing/service.py
@@ -23,24 +23,30 @@ class OrderProcessingService:
 
         try:
             if not db_order:
-                email: EmailSchema = self._email_build_service.build_order_not_found_email(order_id, customer_email)
-                await self._notification_service.send_email(email)
-                raise Exception(f'Заказ №{order_id} не найден!')
+                await self.order_not_found(order_id, customer_email)
 
             await self._order_service.update_status(db_order, OrderStatusEnum.in_processing)
 
             order_processing_successful = await self.do_something_with_order(db_order)
 
             if not order_processing_successful:
-                email: EmailSchema = self._email_build_service.build_order_error_email(db_order)
-                await self._notification_service.send_email(email)
-                await self._order_service.update_status(db_order, OrderStatusEnum.failed)
-                raise Exception(f'Произошла ошибка при обработке заказа №{db_order.id}!')
+                await self.order_processing_failed(db_order)
 
             await self._order_service.update_status(db_order, OrderStatusEnum.completed)
 
         except Exception as ex:
             print(f'<Exception: {ex}>')
+
+    async def order_not_found(self, order_id: int, customer_email: EmailStr) -> None:
+        email: EmailSchema = self._email_build_service.build_order_not_found_email(order_id, customer_email)
+        await self._notification_service.send_email(email)
+        raise Exception(f'Заказ №{order_id} не найден!')
+
+    async def order_processing_failed(self, db_order: OrderModel) -> None:
+        email: EmailSchema = self._email_build_service.build_order_error_email(db_order)
+        await self._notification_service.send_email(email)
+        await self._order_service.update_status(db_order, OrderStatusEnum.failed)
+        raise Exception(f'Произошла ошибка при обработке заказа №{db_order.id}!')
 
     async def do_something_with_order(self, _):
         await sleep(10)
