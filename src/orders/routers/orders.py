43476c32ea181c4cf7ef93bc6cfeba85396c020d
DorLex <dorozhinskiysanya@gmail.com>
2024-04-24 22:39:18 +0300

    test(order): add test_add_order

--- src/orders/routers/orders.py
+++ src/orders/routers/orders.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from starlette import status
 
 from src.accounts.models import UserModel
 from src.accounts.services.auth import get_current_user, verify_token
@@ -24,7 +25,7 @@ async def read_orders(skip: int = 0, limit: int = 100, db: Session = Depends(get
     return orders
 
 
-@router.post('/', response_model=dict[str, str])
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=dict[str, int | str])
 async def add_order(
         order: OrderInSchema,
         db: Session = Depends(get_db),
@@ -40,7 +41,7 @@ async def add_order(
     async with producer as pd:
         await pd.send_and_wait('orders', message)
 
-    return {'message': f'Заказ №{db_order.id} принят на обработку. (Статус: created)'}
+    return {'order_id': db_order.id, 'message': f'Заказ №{db_order.id} принят на обработку. (Статус: created)'}
 
 
 @router.get('/my/', response_model=list[OrderOutSchema])
--- tests/orders/test_orders.py
+++ tests/orders/test_orders.py
@@ -1,4 +1,5 @@
 import pytest
+from aiokafka import AIOKafkaProducer
 from starlette import status
 
 from src.orders.enums import OrderStatusEnum
@@ -8,11 +9,30 @@ from tests.conftest import SessionTest
 
 
 class TestOrdersPositive:
+    order_data = {
+        'title': 'test_order_1',
+        'description': 'test_order_1 description'
+    }
+
+    async def mock_send_and_wait(*args, **kwargs):
+        return True
 
     def test_read_orders(self, client, auth_headers):
         response = client.get('/orders', headers=auth_headers)
         assert response.status_code == status.HTTP_200_OK, response.text
         assert len(response.json()) > 0
+
+    def test_add_order(self, client, auth_headers, monkeypatch):
+        monkeypatch.setattr(AIOKafkaProducer, 'send_and_wait', self.mock_send_and_wait)
+
+        response = client.post('/orders', json=self.order_data, headers=auth_headers)
+        assert response.status_code == status.HTTP_201_CREATED, response.text
+
+        order_id = response.json().get('order_id')
+        with SessionTest() as db:
+            db_order: OrderModel = OrderRepository(db).get_by_id(order_id)
+            assert db_order.title == self.order_data.get('title')
+            assert db_order.description == self.order_data.get('description')
 
     def test_read_my_orders(self, client, auth_headers):
         response = client.get('/orders/my/', headers=auth_headers)
@@ -26,6 +46,14 @@ class TestOrdersPositive:
 
 
 class TestOrdersNegative:
+    incorrect_order_data = {
+        'title': 'test_order_2'
+    }
+
+    def test_add_order(self, client, auth_headers):
+        response = client.post('/orders', json=self.incorrect_order_data, headers=auth_headers)
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY, response.text
+
     def test_update_order_status(self, base_test_order):
         with SessionTest() as db:
             with pytest.raises(ValueError):
