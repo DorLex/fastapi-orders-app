c7f611be440e7e01b826a27c1f6c01cbd0884bb8
Alexander Dorozhinskiy <103284328+DorLex@users.noreply.github.com>
2025-03-26 17:46:51 +0300

    Update utils.py

--- src/kafka_service/utils.py
+++ src/kafka_service/utils.py
@@ -1,9 +1,12 @@
 import json
+from typing import TypeVar
+
+T = TypeVar('T')
 
 
-def serializer(message):
+def serializer(message: T) -> bytes:
     return json.dumps(message).encode('utf-8')
 
 
-def deserializer(message):
+def deserializer(message: bytes) -> T:
     return json.loads(message.decode('utf-8'))
