c18d819655cca48b709b5ec48ddd80a02b568e30
Alexander Dorozhinskiy <103284328+DorLex@users.noreply.github.com>
2025-03-26 17:46:16 +0300

    Update mocks.py

--- tests/orders/mocks.py
+++ tests/orders/mocks.py
@@ -1,13 +1,22 @@
+from types import TracebackType
+from typing import Any, Self
+
+
 class MockKafkaProducer:
-    async def __aenter__(self):
+    async def __aenter__(self) -> Self:
         return self
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(
+        self,
+        exc_type: type[Exception] | None,
+        exc_value: Exception | None,
+        exc_traceback: TracebackType | None,
+    ) -> None:
         pass
 
-    async def send_and_wait(self, *args, **kwargs):
+    async def send_and_wait(self, *args: Any, **kwargs: Any) -> bool:
         return True
 
 
-async def mock_get_producer():
+async def mock_get_producer() -> MockKafkaProducer:
     return MockKafkaProducer()
